4a1f3ca8f36312d3c71b2b8d707d8058f5cc5684
rabbani4u <raabbani.iu31@gmail.com>
2023-06-12 23:38:32 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const url = `https://jsonplaceholder.typicode.com/comments`;
+const getComments = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  const comments = data.slice(0, 20);
+
+  const commentContainer = document.getElementById("comments-container");
+
+  comments.forEach(comment => {
+    const { id, name, email, body } = comment;
+    // console.log(comment);
+    const commentDiv = document.createElement("div");
+    commentDiv.classList.add("comment");
+    commentDiv.innerHTML = `
+    <p><strong>Comment ID: </strong>${id}</p>
+    <p><strong>Name: </strong>${name}</p>
+    <p><strong>Email: </strong>${email}</p>
+    <p><strong>Body: </strong>${body}</p>
+    <hr />
+    `;
+    commentContainer.appendChild(commentDiv);
+  });
+};
+getComments();
